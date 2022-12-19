70f00f7df22a37616ab03dfaece07b6ffbc66a7a
Maria Tworek <mariatworek@MacBook-Pro-Maria.local>
2022-12-19 16:01:44 +0100

    My first initial commit

---
+++ hello.js
@@ -0,0 +1,4 @@
+const firstNameCat ='Fibi';
+const catAge='2,5';
+
+console.log(`Dzień dobry, nazywam się ${firstNameCat} i mam ${catAge} lat.`);
---
+++ main.js
@@ -0,0 +1,7 @@
+const firstName ='Maria';
+const age=27;
+
+
+console.log(firstName);
+console.log(age);
+console.log(`Siema, nazywam się ${firstName} i mam ${age} lat.`);
